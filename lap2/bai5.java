51808d34fe962355056ae0c8e71b4e481667c022
John Doe <nghuyhoang211104@gmail.com>
2024-01-26 10:59:49 +0700

    javaoop

--- lap2/bai5.java
+++ lap2/bai5.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class bai5 {
     public static void main(String[] args) {
         int n,tong = 0;
+        String str = "";
         Scanner sc = new Scanner(System.in);
-        System.out.print("nhap cac so nguyen bat ky: ");
+        System.out.println("nhap cac so nguyen bat ky: ");
         while (tong <= 100) {
             n = sc.nextInt();
             tong += n;
+            str = str + n + " + ";
         }
-        System.out.print("Tong cac so da nhap la: "+tong);
+        str = str.substring(0,str.length() - 2);
+        System.out.print(str +"= "+tong);
         sc.close();
     }
 }
